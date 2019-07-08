4f3129939a0ff6b02ad98210e015c161395843ce
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-07-08 19:23:37 +0600

    Login Page
    registration.php file created

---
+++ Project Codes/registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registration</title>
+</head>
+<body>
+	
+
+<div class="container">
+	
+    <div class="header">
+    	<h2>Register</h2>
+    </div>
+
+    <form action="registration.php" method="post"></form>
+
+</div>
+
+
+</body>
+</html>
