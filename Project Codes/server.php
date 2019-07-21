d3bb06b43d39f142b9e8e89d76a2bc8c96b95bca
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-07-21 15:18:31 +0600

    Bug #11 : Change in myqli

--- Project Codes/server.php
+++ Project Codes/server.php
@@ -9,19 +9,24 @@ session_start();
   $errors = array();
   // connect to Data base
 
+<<<<<<< Updated upstream
   $db = mysql_connect('localhost','root','','menstrualcycle') or die("could not connect to data base");
+=======
+  $db = mysqli_connect('localhost','root','','menstrualcycle') or die("could not connect to data base");
+>>>>>>> Stashed changes
 
 
 //Register users 
 
-  $username =mysql_real_escape_string($db, $_POST['username']);
-  $email =mysql_real_escape_string($db, $_POST['email']);
-  $password_1 =mysql_real_escape_string($db, $_POST['password_1']);
-  $password_2 =mysql_real_escape_string($db, $_POST['password_2']);
+  $username =mysqli_real_escape_string($db, $_POST['username']);
+  $email =mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 =mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 =mysqli_real_escape_string($db, $_POST['password_2']);
 
 
 
   // form validation 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
   if (empty($username)) {
   	array_push($errors, "Username is required")
@@ -43,14 +48,20 @@ session_start();
    if (empty($password_1)) {array_push($errors, "Password is required")};
    if($password_1 != $password_2){array_push($errors,"Passwords need to be the same")};
 >>>>>>> Stashed changes
+=======
+   if (empty($username)) {array_push($errors, "Username is required");}
+   if (empty($email)) {array_push($errors, "Email is required");}
+   if (empty($password_1)) {array_push($errors, "Password is required");}
+   if($password_1 != $password_2){array_push($errors,"Passwords need to be the same");}
+>>>>>>> Stashed changes
 
   //check db for same user name
 
   $user_check_query = "SELECT *FROM user WHERE username = '$username' or email = '$email' LIMIT 1";
 
-  $results = mysql_query($db, $user_check_query);
+  $results = mysqli_query($db, $user_check_query);
 
-  $user = mysql_fetch_assoc($result);
+  $user = mysqli_fetch_assoc($result);
 
   if($user){
 
