6de47458563cf8e79aceb25391c53d8f38fea3c7
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-07-23 01:29:05 +0600

    Bug #9 : Solved the bug in the form link
    In line number 33 the action was registration.php but the include was missing

--- Project Codes/registration.php
+++ Project Codes/registration.php
@@ -5,10 +5,7 @@
 	<meta charset="UTF-8">
 	<title>Registration</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-<<<<<<< Updated upstream
-=======
     
->>>>>>> Stashed changes
 </head>
 <body style="background-color:  #FFFFFF">
 	
@@ -24,20 +21,10 @@
     	<h2>Sign Up Here</h2>
     </div>
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <form action="registration.php" method="post">
-=======
-=======
->>>>>>> Stashed changes
     <form action="registration.php"  method="post">
         <center><img src="img/l2.png" alt="#" class="l1"></center>
 
         <?php include('errors.php') ?>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     	
         <div class="input-group">
         	<label for="username">User Name : </label>
@@ -58,20 +45,13 @@
 
          <div class="input-group">
         	<label for="password">Confirm Password : </label>
-        	<input type="password" name="password_2" required>
+        	<input type="password" name="password_2"  required>
         </div>
 
         <div class="input-group">
         <button type="submit" class="btn" name="reg_user">Submit</button>
         </div>
 
-<<<<<<< Updated upstream
-=======
-        <div class="input-group">
-        <button type="submit" class="btn" name="reg_user">Submit</button>
-        </div>
-
->>>>>>> Stashed changes
 
         <p>Already a user <a href="login.php"><b>Log in</b></a></p>
 
@@ -81,4 +61,4 @@
 
 
 </body>
-</html>
+</html> 
