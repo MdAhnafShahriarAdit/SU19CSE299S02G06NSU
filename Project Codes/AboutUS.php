c13c2c19c077c939ca437d15865d790f63b3260e
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-06 04:27:51 +0600

    Feature #30 : Title Added
    NOBONI title added

--- Project Codes/AboutUS.php
+++ Project Codes/AboutUS.php
@@ -1,9 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NOBONI</title>
+<link href="css/lightbox.css" rel="stylesheet" />
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+     <script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/lightbox.min.js"></script>
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/jquery.flexslider.js" type="text/javascript"></script>
+  
+ <script type="text/javascript">
+     $(function () {
+         SyntaxHighlighter.all();
+     });
+     $(window).load(function () {
+         $('.flexslider').flexslider({
+             animation: "slide",
+             animationLoop: false,
+             itemWidth: 210,
+             itemMargin: 5,
+             minItems: 2,
+             maxItems: 4,
+             start: function (slider) {
+                 $('body').removeClass('loading');
+             }
+         });
+     });
+  </script>
 </head>
+
 <body>
 	
 </body>
