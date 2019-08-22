75cf0a981e1a44fb369f4368cbf37856bd5a56e1
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-22 23:05:39 +0600

    Feature 60 : Footer and navigation created

---
+++ Project Codes/footer.php
@@ -0,0 +1,20 @@
+<div class="footer">
+
+   <ul >
+        <a  href="index.php">Home &ensp;</a></li>
+        <a  href="AboutUS.php">About Us&ensp;</a></li>
+        <a  href="contact.php">Contact Us&ensp;</a></li>
+        <a  href="">Products&ensp;</li>
+        <a  href="">Health Tips&ensp; </a></li>
+        <a  href="">Calender&ensp;</a></li>
+        <a  href="registration.php">Join Us&ensp;</a></li>
+        <a  href="login.php?logout='1'" style="color : red;">Logout&ensp;&ensp;&emsp;&emsp;</a></li>
+      </ul>
+ 
+
+
+
+
+</div>
+
+
