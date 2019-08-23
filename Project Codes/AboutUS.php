2f1940398bced11c3e82797084dd4df5a45f31f7
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-23 22:11:36 +0600

    Feature #30 : Footer Added

--- Project Codes/AboutUS.php
+++ Project Codes/AboutUS.php
@@ -11,7 +11,7 @@
   <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php"><div class="header">
     <div class="logo">
-      <h1><a href="index.php"><img src="Image/2.jpg" height="150" width="150" ; alt=""></a></h1>
+      <h1><a href="index.php"><img src="Image/2.jpg" height="100" width="100" ; alt=""></a></h1>
     </div>
   </div></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +39,13 @@
 <!-- Header Area End -->
 
 <div class="container">
+<div>
+  
+  <p>
+    <img src="Image/AboutUsImage.jpg" alt="">
+  </p>
 
+</div>
 
           <div class="header-content">
             <h2> NOBONI নবণী </h2>
@@ -122,5 +128,95 @@ A trusted source of information</h4>
     
 
 
+      <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+  <div class="f_nav">
+
+
+   <ul >
+       <li > <a  href="index.php">Home </a></li>
+       <li > <a  href="AboutUS.php">About Us</a></li>
+       <li > <a  href="contact.php">Contact Us</a></li>
+       <li > <a  href="">Products</li>
+       <li > <a  href="">Health Tips </a></li>
+       <li > <a  href="">Calender</a></li>
+       <li > <a  href="registration.php">Join Us</a></li>
+        <li ><a  href="login.php?logout='1'" style="color : red;">Logout</a></li>
+      </ul>
+ 
+
+
+
+
+</div>
+<div class="copy">
+      <p class="title"><br><br>©CSE299 Group 6</p>
+    </div>
+  <div class="clear"></div>
+</div>
+</div>
+</div>
+
+
   </body>
 </html>
+
+<style>
+  .ftr-bg{
+  background: #f73c80;
+  border-top: 1px solid rgba(214, 195, 167, 0.34);
+   position: ;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+.footer{
+  padding:3% 0;
+}
+.f_nav{
+  float:left;
+}
+.f_nav li{
+  display: inline-block;
+}
+.f_nav li.active a{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+}
+.f_nav li a{
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  display:block;
+  padding: 8px 18px;
+  color: #805752;
+  font-size: 0.8125em;
+}
+.f_nav li a:hover{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+  -webkit-transition: 0.9s;
+  -moz-transition: 0.9s;
+  -o-transition: 0.9s;
+}
+
+</style>
+
+
