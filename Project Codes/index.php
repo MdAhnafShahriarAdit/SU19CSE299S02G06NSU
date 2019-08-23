8a370eb857faaeb51977e9d34cc7b300b9610e1c
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-23 22:40:47 +0600

    Feature #32 : Changed framework of Home Page
    Framework  changed and new design added

--- Project Codes/index.php
+++ Project Codes/index.php
@@ -1,55 +1,62 @@
-<!DOCTYPE html>
-
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+  <head>
+    <title  > NOBONI </title>
 
-	 <link rel="stylesheet" href="style.css">
-
-	<title>NOBONI</title>
-</head>
-<body>
-	
-  <!--Navigation bar-->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div class="wrap">
-<div class="header">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="style.css">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><div class="header">
     <div class="logo">
-      <h1><a href="index.php"><img src="Images/2.jpg" alt=""></a></h1>
+      <h1><a href="index.php"><img src="Image/2.jpg" height="100" width="100" ; alt=""></a></h1>
     </div>
+  </div></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      
+      <a class="nav-link" href="index.php">Home &ensp;<span class="sr-only"></span></a></li>
+        <a class="nav-link" href="AboutUS.php">About Us&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="contact.php">Contact Us&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="">Products&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="">Health Tips&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="">Calender&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="registration.php">Join Us&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="login.php?logout='1'" style="color : red;">Logout&ensp;&ensp;&emsp;&emsp;<span class="sr-only">(current)</span></a></li>
+      </ul>
+</div>
+</nav>
+</head>
+</html>
+
+<!-- Header Area End -->
+
+<div class="container">
+<div>
+  
+  <p>
+    <img src="Image/AboutUsImage.jpg" alt="">
+  </p>
+
+</div>
+
+          <div class="header-content">
+            <h2> NOBONI নবণী </h2>
+            <p>
+            Our mission is to seamlessly elevate women’s health by leveraging the essential need for period tracking. 
+          </div>
+     
   </div>
 </div>
-</div>
-<div class="nav_bg">
-<div class="wrap">
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav navbar-right">
-             <li><a href=""></a>Home</li>
-            <li><a href=""></a>About Us</li>
-            <li><a href=""></a>Product</li>
-            <li><a href=""></a>Calender</li>
-            <li><a href=""></a>Payment</li>
-            <li><a href=""></a>Health Tips</li>
-            <li><a href=""></a>Contact</li>
-            <li><a href=""></a>Register</li>
-           <li><a href="login.php?logout='1'" style="color: red;">logout</a> <li></li>
-          
-        </ul>
-        </div>
-      </div>
-    </nav>
-    <!--/ Navigation bar-->
-
-     <!--Banner-->
+ 
+<!--Banner-->
     <div class="banner">
       <div class="bg-color">
         <div class="container">
@@ -73,81 +80,166 @@
     <!--/ Banner-->
 
 
-    <!--About Us-->
-    <section id ="aboutUs" class="section-padding">
-      <div class="container">
-        <div class="row">
-          <div class="header-section text-center">
-            <h2>About Us</h2>
-            <p>From super–informative articles to real answers from three perspectives — a peer, a mom, and a health expert — you'll find everything you need to know about periods.
-This is not intended to be medical advice. Everybody is different so please make sure to consult your physician if you're having issues. Do not delay or refrain from seeking professional medical advice from your physician because of something you have read on this site.. <br> We always try to keep our website up to date.<br> We belive in user satisfaction</p>
-            <hr class="bottom-line">
-          </div>
-          <div class="feature-info">
-            <div class="fea">
-              <div class="col-md-4">
-                <div class="heading pull-right">
-                  <h4>Products</h4>
-                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
-                </div>
-                <div class="fea-img pull-left">
-                  <i class="fa fa-css3"></i>
-                </div>
-              </div>
-            </div>
-            <div class="fea">
-              <div class="col-md-4">
-                <div class="heading pull-right">
-                  <h4>Medicines</h4>
-                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
-                </div>
-                <div class="fea-img pull-left">
-                  <i class="fa fa-drupal"></i>
-                </div>
-              </div>
-            </div>
-            <div class="fea">
-              <div class="col-md-4">
-                <div class="heading pull-right">
-                  <h4>Health Care Tips</h4>
-                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
-                </div>
-                <div class="fea-img pull-left">
-                  <i class="fa fa-trophy"></i>
-                </div>
-              </div>
-            </div>
-         </div>
+<!-- Deatils start -->
+
+<div class="who_why">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="who_why_title">
+          <img src="Image/1stepsolution.png" alt="">
+          <h2>One-stop solution</h2>
+        </div>
+        <div class="who_why_content">
+          <p>
+
+
+ NOBONI নবণী is a health website for women that supports them during an entire reproductive period, encompasses accurate cycle predictions, personalized daily health insights and safe community of experts and peers.
+</p>
         </div>
       </div>
-    </section>
-    <!--/ feature-->
+    </div>
+  </div>
+</div>
+<!-- Who and Why area end -->
 
- 
+<!-- Feature start -->
 
-<!-- Footer Start -->
+  <div class="feature-area">
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-md-12"> -->
+          <div class="col-md-4">
+            <div class="single-feature-box">
+              <img src="Image/Founded.png" alt="">
+              <h4>History</h4>
+              <p>NOBONI নবণী was founded July 2019</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="single-feature-box">
+              <img src="Image/peopleCount.png" alt="">
+              <h4>
+             People work at NOBONI নবণী</h4>
+              <p>NOBONI নবণী was created by 2 undergradute student of North South University under the supervision of SHAIKH SHAWON AREFIN SHIMON</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="single-feature-box">
+              <img src="Image/aTrustedSource.png" alt="">
+              <h4>
+A trusted source of information</h4>
+              <p>We use our expertise to provide a trusted source of health information and secure communication for  women.</p>
+            </div>
+          </div>
+        </div>
+      <!-- </div> -->
+    </div>
+  </div>
+
+<!-- Feature end -->
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+    <script defer src="js/fontawesome-all.min.js"></script>
+    <script src="js/custom.js"></script>
+
+    
+
+
+      <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
 <div class="footer">
   <div class="f_nav">
-    <ul>
-     
-             <li class="active"><a href="index.php">Home</a></li>
-            <li class="active"><a href="">About Us</a></li>
-            <li class="active"><a href="">Product</a></li>
-            <li class="active"><a href="">Calender</a></li>
-            <li class="active"><a href="">Payment</a></li>
-            <li class="active"><a href="">Health Tips</a></li>
-            <li class="active"><a href="contact.php">Contact</a></li>
-            <li class="active"><a href="registration.php">Register</a></li>
-           <li class="active"><a href="login.php?logout='1'" style="color: red;">logout</a> <li></li>
-          
-      
-            </ul>
-  </div>
-    <div class="copy">
-      <p class="title"> </p>
+
+
+   <ul >
+       <li > <a  href="index.php">Home </a></li>
+       <li > <a  href="AboutUS.php">About Us</a></li>
+       <li > <a  href="contact.php">Contact Us</a></li>
+       <li > <a  href="">Products</li>
+       <li > <a  href="">Health Tips </a></li>
+       <li > <a  href="">Calender</a></li>
+       <li > <a  href="registration.php">Join Us</a></li>
+        <li ><a  href="login.php?logout='1'" style="color : red;">Logout</a></li>
+      </ul>
+ 
+
+
+
+
+</div>
+<div class="copy">
+      <p class="title"><br><br>©CSE299 Group 6</p>
     </div>
   <div class="clear"></div>
 </div>
+</div>
+</div>
 
 
-<!-- Footer end -->
+  </body>
+</html>
+
+<style>
+  .ftr-bg{
+  background: #f73c80;
+  border-top: 1px solid rgba(214, 195, 167, 0.34);
+   position: ;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+.footer{
+  padding:3% 0;
+}
+.f_nav{
+  float:left;
+}
+.f_nav li{
+  display: inline-block;
+}
+.f_nav li.active a{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+}
+.f_nav li a{
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  display:block;
+  padding: 8px 18px;
+  color: #805752;
+  font-size: 0.8125em;
+}
+.f_nav li a:hover{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+  -webkit-transition: 0.9s;
+  -moz-transition: 0.9s;
+  -o-transition: 0.9s;
+}
+
+</style>
+
+
