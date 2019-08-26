1d2845140149fd34261be579124b40137af3e00a
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-26 22:41:49 +0600

    Feature #51 : Adding calender
    Calender,personal choice , select choice (Regular  ,Irregular , Miss)

--- Project Codes/calender.php
+++ Project Codes/calender.php
@@ -33,6 +33,83 @@
       </ul>
 </div>
 </nav>
+
+
+
+
+
+     <div>
+       
+       <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+
+<!-- Inline CSS based on choices in "Settings" tab -->
+<style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
+
+<!-- HTML Form (wrapped in a .bootstrap-iso div) -->
+<div class="bootstrap-iso">
+ <div class="container-fluid">
+  <div class="row">
+   <div class="col-md-6 col-sm-6 col-xs-12">
+    <form method="post">
+     <div class="form-group ">
+      <label class="control-label " for="date">
+       Date
+      </label>
+      <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+     </div>
+     <div class="form-group ">
+      <label class="control-label " for="textarea">
+       Personal Notes
+      </label>
+      <textarea class="form-control" cols="40" id="textarea" name="textarea" rows="10"></textarea>
+      <span class="help-block" id="hint_textarea">
+       Your notes will be  secured and you can only see it.
+      </span>
+     </div>
+     <div class="form-group ">
+      <label class="control-label " for="select">
+       Select a Choice
+      </label>
+      <select class="select form-control" id="select" name="select">
+       <option value="Regular">
+        Regular
+       </option>
+       <option value="Irregular ">
+        Irregular
+       </option>
+       <option value="Miss">
+        Miss
+       </option>
+      </select>
+     </div>
+     <div class="form-group">
+      <div>
+       <button class="btn btn-primary " name="submit" type="submit">
+        Submit
+       </button>
+      </div>
+     </div>
+    </form>
+   </div>
+  </div>
+ </div>
+</div>
+
+
+
+     </div>
+
+
+
+
+
+
+
+
+
+
+
 </head>
 </html>
 
@@ -59,6 +136,27 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
     <script defer src="js/fontawesome-all.min.js"></script>
     <script src="js/custom.js"></script>
+    <!-- Extra JavaScript/CSS added manually in "Settings" tab -->
+<!-- Include jQuery -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+<script>
+    $(document).ready(function(){
+        var date_input=$('input[name="date"]'); //our date input has the name "date"
+        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+        date_input.datepicker({
+            format: 'mm/dd/yyyy',
+            container: container,
+            todayHighlight: true,
+            autoclose: true,
+        })
+    })
+</script>
+
 
     
 
